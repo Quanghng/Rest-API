89c45b735c67134797453cc376c34f067ad63a40
Quanghng <90704201+Quanghng@users.noreply.github.com>
2024-03-13 11:59:25 +0100

    Setup mongoDB

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -19,4 +20,14 @@ const server = http.createServer(app);
 
 server.listen(8080 , () => {
   console.log('Server running on http://localhost:8080/');
+});
+
+const MONGO_RUL ='mongodb+srv://quanghng:lOa8o2lsrohfM6tT@restapi.sq6zn97.mongodb.net/?retryWrites=true&w=majority&appName=restapi'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_RUL)
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection established successfully');
+}).on('error', (error) => {
+  console.log('MongoDB connection error:', error);
 });
